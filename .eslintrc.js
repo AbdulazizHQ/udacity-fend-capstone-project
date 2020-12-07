9705fc768450b4e2f22c122635ba954b512114ac
Abdulaziz Alqahtani <a.alqahtani@nicsep.gov.sa>
2020-12-07 23:03:28 +0300

    intial setup

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+// From https://github.com/linesh-simplicity/eslint-config-udacity
+
+
+// eslint-disable-next-line no-undef
+module.exports = {
+   env: {
+      browser: true,
+      es6: true,
+      jquery: true,
+      jasmine: true,
+   },
+   plugins: [
+      'import',
+   ],
+   rules: {
+      'no-trailing-spaces': 'error',
+      indent: ['warn', 3],
+      'unicode-bom': ['error', 'never'],
+      'no-warning-comments': 'warn',
+      semi: 'error',
+      'no-extra-semi': 'error',
+      'no-new-wrappers': 'error',
+      'guard-for-in': 'error',
+      'no-multi-str': 'error',
+      'no-array-constructor': 'error',
+      'no-new-object': 'error',
+      camelcase: 'error',
+      'brace-style': 'error',
+      'object-curly-spacing': 'error',
+      'array-bracket-spacing': 'error',
+      quotes: ['error', 'single'],
+      'no-undef': 'warn',
+   },
+};
---
+++ README.md
@@ -0,0 +1,14 @@
+Evaluate a news article with Natural Language Processing
+========================================================
+
+
+# Project Overview
+This project provides a web interface for commuinicating with a NLP API for sentiment analysis for news articles
+
+# Usage
+  1. Create your `.env` file in project root directory and supply your own API key under `API_KEY`
+  2. `$ npm install`
+  3. `$ npm start`
+
+# Testing
+  `$ npm test`
